381625e3edf0127ed9fd690f97a6e645a4a5f529
Ryan Nordeen <ryannordeen@gmail.com>
2022-09-26 21:16:15 -0500

    displaying mod settings

--- modlink.py
+++ modlink.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import os
+import json
 from random import randint
 
 from PyQt5.QtWidgets import (
@@ -26,6 +27,8 @@ colors = [("Red",            "#FF0000"),
 
 modArchive = dict()
 
+TMP_FILE='tmp.config.json'
+
 def get_rgb_from_hex(code):
     code_hex = code.replace("#", "")
     rgb = tuple(int(code_hex[i:i+2], 16) for i in (0, 2, 4))
@@ -39,32 +42,34 @@ class Worker( QObject ):
         for i in range( 5 ):
             sleep( 1 )
             self.progress.emit( i + 1 )
-            
+
         self.finished.emit()
 
 class ConfigBuilder( Worker ):
     def __init__( self, archivePath ):
         super( ConfigBuilder, self ).__init__()
         self._archivePath = archivePath
-        self._fname = 'tmp.config.json'
-        
+        self._fname = TMP_FILE
+
     def run( self ):
         global modArchive
 
         modArchive = dict()
-        
+
         for i, fname in enumerate( fsutil.yieldall( self._archivePath, '*' ) ):
             modArchive[ fname ] = dict()
             modArchive[ fname ][ 'linked' ] = False
-            #self.progress.emit( i + 1 )
-            
+            self.progress.emit( i + 1 )
+
+        with open( self._fname, 'w' ) as f:
+            json.dump( modArchive, f, indent = 4, sort_keys = False )
         self.finished.emit()
 
 
 class ReadOnlyDelegate( QStyledItemDelegate ):
     def createEditor( self, parent, option, index ):
         return
-    
+
 class Window( QMainWindow, Ui_MainWindow ):
     def __init__( self, parent = None ):
         super().__init__(parent)
@@ -75,14 +80,14 @@ class Window( QMainWindow, Ui_MainWindow ):
         self.threads = dict()
 
     def create_worker( self, _class, *args, **kwargs ):
-        
+
         thread = QThread()
         if thread not in self.threads.keys():
             self.threads[ thread ] = []
-        
+
         worker = _class( *args, **kwargs )
         self.threads[ thread ].append( worker )
-        
+
         worker.moveToThread( thread )
         thread.started.connect( worker.run )
         worker.finished.connect( thread.quit )
@@ -91,10 +96,11 @@ class Window( QMainWindow, Ui_MainWindow ):
         worker.progress.connect( self.reportProgress )
 
         thread.start()
+        return worker
 
     def reportProgress( self, n ):
         print( n )
-        
+
     def connectSignalSlots(self ):
         self.actionNew.triggered.connect( self.new_config )
         self.actionLoad.triggered.connect( self.load )
@@ -102,34 +108,68 @@ class Window( QMainWindow, Ui_MainWindow ):
         self.actionExit.triggered.connect( self.close )
 
     def initTable(self):
+        global modArchive        
+        print( 'init Table' )
         self.readonly_delegate = ReadOnlyDelegate( self.tableWidget )
+
+        is_empty = len( modArchive ) == 0
         
-        self.tableWidget.setRowCount( len( colors ) )
-        self.tableWidget.setColumnCount( len( colors[ 0 ] ) +1 )
-        self.tableWidget.setHorizontalHeaderLabels( ["Name", "Hex Code", "Colors" ] )
+        if is_empty and os.path.exists( TMP_FILE ):
+            print( 'Reading previous temp file' )
+            with open( TMP_FILE, 'r' ) as f:
+                modArchive = json.load( f )
 
-        # Init data
-        for i, (name, code ) in enumerate( colors ):
-            item_name = QTableWidgetItem( name )
-            #item_name.setFlags( item_name.flags() & ~Qt.ItemIsEnabled ) #Enable
-            #item_name.setFlags( item_name.flags() & ~Qt.ItemIsEditable )
+        if is_empty:
+            return
 
+        rows = len( modArchive )
+        first_item = next( iter( modArchive ) )
+        columns = len( modArchive[ first_item ] ) + 1
             
-            
-            item_code = QTableWidgetItem( code )
-            item_color = QTableWidgetItem()
-            item_color.setBackground( get_rgb_from_hex( code ) )
-            item_color.setFlags( item_color.flags() | Qt.ItemIsUserCheckable )
-            item_color.setCheckState( Qt.Unchecked )
-            
-            self.tableWidget.setItem( i, 0, item_name )
-            self.tableWidget.setItem( i, 1, item_code )
-            self.tableWidget.setItem( i, 2, item_color )
-            
+        self.tableWidget.setRowCount( rows )
+        self.tableWidget.setColumnCount( columns + 1 )
+        self.tableWidget.setHorizontalHeaderLabels( ["Path", "Linked", "Enabled" ] )
+
+        # Init modArchive
+        for i, (path, settings) in enumerate( modArchive.items() ):
+        #for i, (name, code ) in enumerate( colors ):
+            item_path = QTableWidgetItem( path )
+            linked = settings[ 'linked' ]
+            if linked:
+                state = Qt.Checked
+                link_str = 'True'
+            else:
+                state = Qt.Unchecked
+                link_str = 'False'                
+
+            item_linked = QTableWidgetItem( link_str )
+            item_enabled = QTableWidgetItem()
+            item_enabled.setFlags( item_enabled.flags() | Qt.ItemIsUserCheckable )
+
+            item_enabled.setCheckState( state )
+            self.tableWidget.setItem( i, 0, item_path )
+            self.tableWidget.setItem( i, 1, item_linked )
+            self.tableWidget.setItem( i, 2, item_enabled )
+
 
         self.tableWidget.setItemDelegateForColumn( 0, self.readonly_delegate )
         self.tableWidget.setItemDelegateForColumn( 1, self.readonly_delegate )
+        self.tableWidget.cellChanged.connect( self.onCellChanged )
 
+    def onCellChanged( self, row, column ):
+        print( row, column )
+        item = self.tableWidget.item( row, column )
+        print( item )
+        lastState = item.data( Qt.UserRole )
+        currentState = item.checkState()
+        if lastState != currentState:
+            print( 'changed' )
+
+        path    = self.tableWidget.item( row, 0 )
+        linked  = self.tableWidget.item( row, 1 )
+        enabled = self.tableWidget.item( row, 2 ) 
+        print( path.text(), linked.text(), enabled.checkState()==Qt.Checked )
+            
 
     def new_config( self ):
         global modArchive
@@ -143,10 +183,11 @@ class Window( QMainWindow, Ui_MainWindow ):
 
         # Create a placeholder file name
         self.config_file = 'tmp.config.json'
-        self.create_worker( ConfigBuilder, archivePath = self.archivePath )       
+        worker = self.create_worker( ConfigBuilder, archivePath = self.archivePath )
+        worker.finished.connect( self.initTable )
         print( 'ModArchive = [{}]'.format( modArchive ) )
-            
-        
+
+
     def load( self ):
         print( 'load' )
         path = QFileDialog.getOpenFileName( self, 'Mod Configuration File', '*.*' )[0]
@@ -156,7 +197,7 @@ class Window( QMainWindow, Ui_MainWindow ):
     def save( self ):
         print( 'Save' )
         pass
-    
+
     def createLinks(self):
         print( 'ModArchive = [{}]'.format( modArchive ) )
         pass
